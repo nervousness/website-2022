2520ffee75738060e81c5637f1c10dbe74334d3b
swanine <haohao1500@foxmail.com>
2022-04-10 09:19:45 +0800

    🔧 chore: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue"
+import SwanineIndex from "./views/index";
+
+export default defineComponent({
+  render() {
+    return (
+      <>
+        <SwanineIndex></SwanineIndex>
+      </>
+    )
+  }
+})
